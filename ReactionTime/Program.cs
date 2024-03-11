d6798c52ee4fa163acc840c6d8d3b39e8d528ebe
sunryze <lometon592@gmail.com>
2024-03-11 09:54:57 -0400

    first commit

---
+++ ReactionTime/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+var sw = new Stopwatch();
+Random random = new Random();
+
+void SetConsoleBackgroundColor(ConsoleColor color) {
+    Console.BackgroundColor = color;
+    for (int i = 0; i < Console.WindowHeight; i++)
+    {
+        Console.WriteLine(new string(' ', Console.WindowWidth));
+    }
+}
+
+void WriteCenteredText(string text) {
+    Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.CursorTop);
+    Console.WriteLine(text);
+}
+
+void Main() {
+    while (true) {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Black;
+        WriteCenteredText("Thank you for running the Reaction Time Test.");
+        WriteCenteredText("Press any key to start the test.");
+        Console.ReadKey();
+
+        Console.Clear();
+        SetConsoleBackgroundColor(ConsoleColor.Red);
+        Thread.Sleep(random.Next(3000, 6000));
+        SetConsoleBackgroundColor(ConsoleColor.Green);
+        sw.Start();
+        Console.ReadKey();
+        sw.Stop();
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+        WriteCenteredText($"Your reaction time was {sw.ElapsedMilliseconds}ms.");
+        WriteCenteredText($"Press any key to play again.");
+        Console.ReadKey();
+    }
+}
+
+Main();
